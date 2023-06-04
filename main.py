11ea6ae95e4e7d982a61ee949dfeb47c1c1b6fd1
pr0ph3t <dsneddon00@gmail.com>
2023-06-03 20:20:00 -0600

    preparing for data usage for OPENAI

--- main.py
+++ main.py
@@ -18,12 +18,47 @@ def main():
 
     # prepare API key
     args = getArgumentsFromUser()
+    
+    # OpenAPI Client
+    openai.api_key = args.api_key
+
     print("Your API Key is: " + str(args.api_key) + "\n")
 
     # prepare book for data parsing
     # using default path as shown in the Git Repo
-    bookData = loadBook("data/epdf.pub_mushrooms-demystified.txt")
+    bookData = loadBook("data/mushrooms_data_only.txt")
     print("Book data successfully loaded.\n")
+
+    # prepare OpenAI usage
+    max_tokens = 4096  # Maximum number of tokens per API call
+    stop_sequence = "\n\n"  # Sequence to stop the response at
+
+    # Process the book text in chunks
+    offset = 0
+    while offset < len(bookData):
+        # Generate the response using the ChatGPT API
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=bookData[offset : offset + max_tokens],
+            temperature=0,
+            max_tokens=max_tokens,
+            stop=stop_sequence,
+        )
+    
+        # Extract the relevant information from the response
+        generated_text = response.choices[0].text.strip()
+        extracted_info = json.loads(generated_text)
+    
+        # Append the extracted information to the dataset
+        dataset.append(extracted_info)
+    
+        # Update the offset for the next chunk
+        offset += max_tokens
+
+    # Save the dataset as a JSON file
+    dataset_path = "output/mushrooms_dataset.json"
+    with open(dataset_path, "w") as file:
+        json.dump(dataset, file, indent=2)
 
     return
 
